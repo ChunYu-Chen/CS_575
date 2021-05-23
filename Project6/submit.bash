c842429a987145f6decfe801d64565c90df9aba8
ChunYu-Chen <sam8952526@gmail.com>
2021-05-22 19:13:52 -0700

    first

---
+++ Project6/submit.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J MatrixMul
+#SBATCH -A cs475-575
+#SBATCH -p class
+#SBATCH --gres=gpu:1
+#SBATCH -o first.out
+#SBATCH -e first.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=chench6@oregonstate.edu
+for t in 1 8 64 256 1024 2048 4096 8192
+do
+        for s in 8 16 32 64 128 256 512
+        do
+        g++ -o first first.cpp -DNMB=$t -DLOCAL_SIZE=$s /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
+        ./first
+        done
+done
+
